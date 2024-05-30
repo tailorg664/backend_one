39a3845e5159aea4f705417a4fd59824d753a734
godevx <tailorg664@gmail.com>
2024-05-30 20:24:15 +0530

    Update user.routes.js with new endpoints and middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,49 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import {
+   loginUser,
+   logoutUser,
+   registerUser,
+   refreshAccessToken,
+   changeCurrentPassword,
+   getCurrentUser,
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage,
+   getUserChannelProfile,
+   getWatchHistory,
+} from "../controllers/user.controller.js";
 //importing upload from multer middleware
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 const router = Router();
 
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
+   upload.fields([
+      {
+         name: "avatar",
+         maxCount: 1,
+      },
+      {
+         name: "coverImage",
+         maxCount: 1,
+      },
+   ]),
+   registerUser
 );
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-router.route("/logout").post(verifyJwt ,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+router
+   .route("/avatar")
+   .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+   .route("/cover-image")
+   .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 export default router;
